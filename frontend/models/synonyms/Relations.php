1f28e8bffd936d48f3e77affba02827f6d2be763
mgnedash <gnedasch2011@yandex.ru>
2020-09-21 10:32:37 +0300

    css

---
+++ frontend/models/synonyms/Relations.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\models\synonyms;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+
+/**
+ * This is the model class for table "relations".
+ *
+ * @property int $id
+ * @property int $id_synonymys
+ * @property int $id_relations_synonymys
+ */
+class Relations extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'relations';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id_synonymys', 'id_relations_synonymys'], 'integer'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'id_synonymys' => 'Id Synonymys',
+            'id_relations_synonymys' => 'Id Relations Synonymys',
+        ];
+    }
+
+    public static function findRelations($ids)
+    {
+        $res = self::find()
+            ->where(['id_synonymys' => $ids])
+            ->asArray()
+            ->all();
+
+        if ($res) {
+            $res = ArrayHelper::getColumn($res, 'id_relations_synonymys');
+        }
+
+
+        return $res;
+    }
+}
---
+++ frontend/models/synonyms/Synonymys.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace app\models\synonyms;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+
+/**
+ * This is the model class for table "synonymys".
+ *
+ * @property int $id
+ * @property string $name
+ */
+class Synonymys extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'synonymys';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+
+
+    public static function findAllSynonyms($query)
+    {
+        $query = 'жизнь';
+
+        $res = self::find()
+            ->where(['name' => $query])
+            ->asArray()
+            ->all();
+
+        if ($res) {
+            $ids = ArrayHelper::getColumn($res, 'id');
+        }
+
+        $relationsIds = Relations::findRelations($ids);
+
+        $synonymsArr = self::find()
+            ->where(['id' => $relationsIds])
+            ->asArray()
+            ->all();
+
+
+        return $synonymsArr;
+    }
+
+}
